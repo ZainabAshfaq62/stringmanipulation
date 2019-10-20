c1f142b30517a615307f3c3f41d4db35e16087d5
ZainabAshfaq62 <55431070+ZainabAshfaq62@users.noreply.github.com>
2019-10-20 00:39:45 -0700

    inverse string
     VB code on how to write any string inversely

---
+++ inverse string.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, finalstr As String
+        Dim count As Integer
+        Dim nextchar As Char
+
+        str1 = ""
+        finalstr = ""
+        count = 0
+        nextchar = ""
+
+        Console.Write("enter your string: ")
+        str1 = Console.ReadLine
+
+        For count = 1 To Len(str1)
+            nextchar = Mid(str1, count, 1)
+            finalstr = nextchar & finalstr
+
+
+        Next
+        Console.WriteLine("the inverse string is: " & finalstr)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
