569aa7b5b68aef65ba68c93e206b8bd0f8589044
ZainabAshfaq62 <55431070+ZainabAshfaq62@users.noreply.github.com>
2019-10-20 01:04:37 -0700

    number of vowels
    how to count the number of vowels in a string

---
+++ vowelcount.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1 As String
+        Dim count, vcount As Integer
+        Dim nextchar As Char
+
+        str1 = ""
+        count = 0
+        nextchar = ""
+        vcount = 0
+
+        Console.WriteLine("ENTER YOUR STRING: ")
+        str1 = Console.ReadLine
+
+        For count = 1 To Len(str1)
+            nextchar = Mid(str1, count, 1)
+            If UCase(nextchar) = "A" Or UCase(nextchar) = "E" Or UCase(nextchar) = "I" Or UCase(nextchar) = "O" Or UCase(nextchar) = "U" Then
+                vcount = vcount + 1
+
+            End If
+        Next
+        Console.Write("the number of vowels are: " & vcount)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
